790e105b560d39ad39b11f8253bc3f8ad3f821bc
Max Kreminski <maxkreminski@gmail.com>
2017-01-18 08:12:21 -0800

    Smarter match strategy based on incremental matching
    Also adds new patterns involving adjectives, 'but', and 'not'.

--- index.js
+++ index.js
@@ -44,6 +44,11 @@ var INITIAL_ANY = 0;
 var INITIAL_CONSONANT = 1;
 var INITIAL_VOWEL = 2;
 
+var STATE_SUBJ = 0;
+var STATE_VERB = 1;
+var STATE_OBJ = 2;
+var STATE_DONE = 3;
+
 // patterns
 
 var subjectPatterns = [
@@ -68,6 +73,14 @@ var subjectPatterns = [
 var verbObjectPatterns = [
   // ...is adjective
   {verb: ['Copula'], object: ['Adj']},
+  // ...is adjective and adjective
+  {verb: ['Copula'], object: ['Adj', 'And', 'Adj']},
+  // ...is not adjective
+  {verb: ['Copula'], object: ['Not', 'Adj']},
+  // ...is adjective but adjective
+  {verb: ['Copula'], object: ['Adj', 'But', 'Adj']},
+  // ...is adjective but not adjective
+  {verb: ['Copula'], object: ['Adj', 'But', 'Not', 'Adj']},
   // ...is verbing
   {verb: ['Copula'], object: ['Gerund']},
   // ...is the object
@@ -81,9 +94,9 @@ var verbObjectPatterns = [
   // ...is in my adjective object (DISABLED)
   //{verb: ['Copula'], object: ['Prep', 'Possessive', 'Adj', 'Noun']},
   // ...can verb
-  {verb: ['Modal', 'Verb'], object: []},
+  {verb: ['Modal', 'Infinitive'], object: []},
   // ...can verb the object
-  {verb: ['Modal', 'Verb'], object: ['Article', 'Noun']},
+  {verb: ['Modal', 'Infinitive'], object: ['Article', 'Noun']},
   // ...verbs adverbially (DISABLED)
   //{verb: ['Verb', 'Adverb'], object: []},
   // ...verbs the object
@@ -124,7 +137,9 @@ for (var i = 0; i < subjectPatterns.length; i++){
 var knownWords = {
   // special "glue" words that constitute their own parts of speech for our purposes
   and: {tag: {And: true}},
+  but: {tag: {But: true}},
   not: {tag: {Not: true}},
+  yet: {tag: {But: true}},
 
   // determiners (more added below)
   the: {tag: {Det: true, Article: true}},
@@ -228,7 +243,6 @@ function classify(word){
   else if (contains(['CC','DT','PDT','PP$','PRP'], word.initTag)){
     // If the word's initTag is "fully enumerated", all legal words for that tag should be known.
     // Thus, if we get here, it might be worth logging the word to see if we've missed any legal words.
-    word.tag = {};
   }
   else {
     if (word.lexTags && !contains(word.lexTags, word.initTag)){
@@ -239,17 +253,22 @@ function classify(word){
       word = Object.assign(word, clone(info));
     }
   }
+  // specially label infinitive verb forms (so we can use them with modals)
+  if (word.tag.Verb && word.count === COUNT_PLURAL){
+    word.tag.Infinitive = true;
+  }
   return word;
 }
 
-function taggedTokenToWord(taggedToken){
+function taggedTokenToWord(taggedToken, index){
   var text = taggedToken[0];
   var normal = normalize(text);
   return {
     text: text,
     initTag: taggedToken[1],
     normal: normal,
-    lexTags: lexicon[normal]
+    lexTags: lexicon[normal],
+    index: index
   };
 }
 
@@ -263,6 +282,17 @@ function wordify(text){
 }
 
 // pattern matching
+
+function createMatcher(pattern){
+  return {
+    pattern: [pattern.subject, pattern.verb, pattern.object],
+    state: STATE_SUBJ,
+    patternIdx: 0,
+    requiredCount: COUNT_ANY,
+    requiredInitial: INITIAL_ANY,
+    words: []
+  };
+}
 
 function hasRequiredCount(word, requiredCount){
   if (requiredCount === COUNT_ANY || !word.count){
@@ -285,6 +315,66 @@ function hasRequiredInitial(word, requiredInitial){
   }
 }
 
+function shouldAccept(matcher, word){
+  var targetTag = matcher.pattern[matcher.state][matcher.patternIdx];
+  return word.tag[targetTag] &&
+         hasRequiredCount(word, matcher.requiredCount) &&
+         hasRequiredInitial(word, matcher.requiredInitial) &&
+         Math.random() < 0.8;
+}
+
+function pushWord(matcher, word){
+  if (matcher.state === STATE_DONE) return matcher; // don't push more words onto a finished match
+
+  matcher.words.push(word);
+  matcher.patternIdx += 1;
+  matcher.requiredInitial = word.initial || INITIAL_ANY;
+  if (word.count){
+    matcher.requiredCount = word.count;
+  }
+
+  // maybe advance state
+  if (matcher.patternIdx >= matcher.pattern[matcher.state].length){
+    matcher.state += 1;
+    matcher.patternIdx = 0;
+    if (matcher.state === STATE_OBJ){
+      matcher.requiredCount = COUNT_ANY;
+    }
+  }
+
+  return matcher;
+}
+
+// tie it all together
+
+function markSentence(words){
+  var finishedMatchers = [];
+  var openMatchers = patterns.map(createMatcher);
+  for (var i = 0; i < words.length; i++){
+    var word = words[i];
+    var remaining = openMatchers;
+    openMatchers = [];
+    for (var j = 0; j < remaining.length; j++){
+      var matcher = remaining[j];
+      var pile = openMatchers;
+      if (shouldAccept(matcher, word)){
+        matcher = pushWord(matcher, word);
+        if (matcher.state === STATE_DONE){
+          pile = finishedMatchers;
+        }
+      }
+      pile.push(matcher);
+    }
+    if (openMatchers.length === 0) break; // all matchers finished successfully!
+  }
+  var matches = finishedMatchers.map(m => m.words);
+  var matchedWords = matches.length > 0 ? randNth(matches) : [];
+  for (i = 0; i < matchedWords.length; i++){
+    matchedWords[i].marked = true;
+  }
+  return words;
+}
+
 function logWord(word){
   var columns = [word.text,
                  word.initTag,
@@ -292,82 +382,6 @@ function logWord(word){
                  (word.lexTags || []).join(','),
                  word.count];
   console.log(columns.join(' | '));
-}
-
-function shouldAccept(word, tag, count, initial){
-  return word.tag[tag] &&
-         hasRequiredCount(word, count) &&
-         hasRequiredInitial(word, initial) &&
-         Math.random() < 0.9;
-}
-
-function matchWordSequence(words, pattern){
-  words = clone(words);
-  var wordIdx = 0;
-  var word;
-  var patternIdx;
-  var requiredCount = COUNT_ANY;
-  var requiredInitial = INITIAL_ANY;
-
-  // subject
-  patternIdx = 0;
-  while (patternIdx < pattern.subject.length){
-    word = words[wordIdx];
-    if (!word) return null; // no more words to try!
-    if (shouldAccept(word, pattern.subject[patternIdx], requiredCount, requiredInitial)){
-      word.marked = true;
-      patternIdx += 1;
-      if (word.count){
-        requiredCount = word.count;
-      }
-      if (word.initial){
-        requiredInitial = word.initial;
-      } else {
-        requiredInitial = INITIAL_ANY;
-      }
-    }
-    wordIdx += 1;
-  }
-
-  // verb
-  var isModal = pattern.verb[0] === 'Modal';
-  if (isModal){
-    requiredCount = COUNT_PLURAL;
-  }
-  patternIdx = 0;
-  while (patternIdx < pattern.verb.length){
-    word = words[wordIdx];
-    if (!word) return null; // no more words to try!
-    if (shouldAccept(word, pattern.verb[patternIdx], requiredCount) &&
-        !(isModal && word.tag.PastTense)){
-      word.marked = true;
-      patternIdx += 1;
-    }
-    wordIdx += 1;
-  }
-
-  // object
-  requiredCount = COUNT_ANY;
-  patternIdx = 0;
-  while (patternIdx < pattern.object.length){
-    word = words[wordIdx];
-    if (!word) return null; // no more words to try!
-    if (shouldAccept(word, pattern.object[patternIdx], requiredCount, requiredInitial)){
-      word.marked = true;
-      patternIdx += 1;
-      if (word.count){
-        requiredCount = word.count;
-      }
-      if (word.initial){
-        requiredInitial = word.initial;
-      } else {
-        requiredInitial = INITIAL_ANY;
-      }
-    }
-    wordIdx += 1;
-  }
-
-  return words; // if we've gotten this far, all the words were found
 }
 
 function writePoemifiedText(node, words){
@@ -399,28 +413,23 @@ function writePoemifiedText(node, words){
 function poemify(selector){
   var nodes = document.querySelectorAll(selector);
   for (var i = 0; i < nodes.length; i++){
-    // get a series of words from the node's text
+    // parse the node's text into a series of words
     var node = nodes[i];
     if (isEmptyOrWhitespace(node.innerText)) continue; // bail out early if there isn't any text
     console.log(node.innerText);
     var words = wordify(node.innerText);
 
     // mark words to keep (i.e. not black out)
-    var matchedWords = null;
     var attempts = 0;
-    while (!matchedWords && attempts < 20){
-      var pattern = randNth(patterns);
-      matchedWords = matchWordSequence(words, pattern);
-      if (matchedWords){
-        console.log(pattern.subject.concat(pattern.verb).concat(pattern.object).join(','));
-        var marked = (matchedWords || []).filter(w => w.marked);
+    while (attempts < 5){
+      words = markSentence(words);
+      var marked = words.filter(w => w.marked);
+      if (marked.length > 0){
         console.log(marked.map(w => w.text).join(' '));
         marked.map(logWord);
+        break;
       }
       attempts += 1;
-    }
-    if (matchedWords){
-      words = matchedWords;
     }
 
     // write text back into the node with most words blacked out
